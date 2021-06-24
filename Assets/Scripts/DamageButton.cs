2d81731e81af53a38655e7c4203b4411886faf3a
Sbor77 <sborovkov82@mail.ru>
2021-06-24 17:17:21 +0300

    Initial commit

---
+++ Assets/Scripts/DamageButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(HealthCounter))]
+public class DamageButton : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private Button _cure;
+
+    private HealthCounter _healthCounter;
+    private float _curePoints = 10f;
+
+    private void Start()
+    {
+        _healthCounter = GetComponent<HealthCounter>();
+    }
+
+    public void OnButtonClick()
+    {
+        _healthCounter.Damage(_curePoints);
+    }
+}
---
+++ Assets/Scripts/HealthCounter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthCounter : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private float _currentHealth;
+
+    public float CurrentHealth => _currentHealth;
+
+    private void Start()
+    {
+        _currentHealth = _slider.value;
+    }
+
+    public void Cure(float healthPoints)
+    {
+        _currentHealth += healthPoints;
+
+        if (_currentHealth > _slider.maxValue)
+            _currentHealth = _slider.maxValue;
+    }
+
+    public void Damage(float healthPoints)
+    {
+        _currentHealth -= healthPoints;
+
+        if (_currentHealth < _slider.minValue)
+            _currentHealth = _slider.minValue;
+    }
+}
