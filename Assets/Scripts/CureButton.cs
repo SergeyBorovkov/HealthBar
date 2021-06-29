70a654ab5fd50d9d508ff37d999a9faf3fd8afcc
Sbor77 <sborovkov82@mail.ru>
2021-06-30 00:26:34 +0300

    Component resort

--- Assets/Scripts/CureButton.cs
+++ Assets/Scripts/CureButton.cs
@@ -3,22 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(HealthCounter))]
 public class CureButton : MonoBehaviour
 {
-    [SerializeField] private Slider _slider;
-    [SerializeField] private Button _cure;
-
-    private HealthCounter _healthCounter;
+    [SerializeField] private Slider _slider;    
+    [SerializeField] private HealthCounter _health;
+        
     private float _curePoints = 10f;
-
-    private void Start()
-    {
-        _healthCounter = GetComponent<HealthCounter>();
-    }
-
+    
     public void OnButtonClick()
     {
-        _healthCounter.Cure(_curePoints);
+        _health.Cure(_curePoints);
     }    
 }
---
+++ Assets/Scripts/Healthbar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private HealthCounter _health;
+    private float _speedRatio = 5f;
+
+    void Update()
+    {
+        _slider.value = Mathf.MoveTowards(_slider.value, _health.CurrentHealth, _speedRatio * Time.deltaTime);
+    }
+}
